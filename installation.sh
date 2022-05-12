392e7871c4fce6b40979cc1cbe48198e6b9c2355
yuvalby <yuvime12@gmail.com>
2022-05-12 20:42:29 +0300

    added bash script

---
+++ installation.sh
@@ -0,0 +1,9 @@
+sudo apt install dnsmasq hostapd iwconfig scapy
+sudo apt install hostapd
+sudo apt install iwconfig
+sudo apt install scapy
+sudo apt install nodejs
+sudo npm install -g nodemon
+cd captive_portal
+sudo npm install
+cd ..
